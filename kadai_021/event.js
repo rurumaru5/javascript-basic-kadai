30e93906866b4559f08d7091ad2316d5e50510ce
rurumaru5 <152298039+rurumaru5@users.noreply.github.com>
2024-01-16 16:23:53 +0900

    kadai_021のアップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+// btnとtextいうidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+// ボタンをクリックしたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+      text.textContent = 'ボタンをクリックしました';
+    }, 2000);
+});
